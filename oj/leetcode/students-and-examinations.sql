b6510a73a9fdb7ad15f96ca81ccd4c127d5967dd
JAHID38 <zahidmuhammad095@gmail.com>
2023-08-16 13:47:19 +0600

    students-and-examinations

---
+++ oj/leetcode/students-and-examinations.sql
@@ -0,0 +1,15 @@
+/* Write your PL/SQL query statement below */
+SELECT
+    S.student_id,
+    S.student_name,
+    Sub.subject_name,
+    COUNT(E.student_id) attended_exams
+FROM
+    Students S
+        CROSS JOIN
+    Subjects Sub
+        LEFT JOIN
+    Examinations E ON E.student_id = S.student_id
+        AND E.subject_name = Sub.subject_name
+GROUP BY S.student_id , Sub.subject_name
+ORDER BY S.student_id , Sub.subject_name;
