9354a4a270f97d95ef88b1080406a6672ec5e0fd
jahid.shadhin <jahid@shadhinmusic.com>
2024-04-15 10:04:19 +0600

    ScoreOfAString_3110

---
+++ oj/leetcode/easy/ScoreOfAString_3110.java
@@ -0,0 +1,12 @@
+package oj.leetcode.easy;
+
+public class ScoreOfAString_3110 {
+    public int scoreOfString(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length()-1; i++)
+        {
+            sum += Math.abs(s.charAt(i) - s.charAt(i+1));
+        }
+        return sum;
+    }
+}
